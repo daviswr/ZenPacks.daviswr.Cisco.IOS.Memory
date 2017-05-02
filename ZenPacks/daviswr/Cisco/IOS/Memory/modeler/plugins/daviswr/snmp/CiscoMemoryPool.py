8f1131cc78740d4c1311bdf63e7ca6ed63dc05d7
Wes Davis <wesley.r.davis@gmail.com>
2017-05-01 22:11:26 -0400

    PEP 8 compliance

--- ZenPacks/daviswr/Cisco/IOS/Memory/modeler/plugins/daviswr/snmp/CiscoMemoryPool.py
+++ ZenPacks/daviswr/Cisco/IOS/Memory/modeler/plugins/daviswr/snmp/CiscoMemoryPool.py
@@ -9,6 +9,7 @@ from Products.DataCollector.plugins.CollectorPlugin \
 from Products.DataCollector.plugins.DataMaps \
     import MultiArgs, RelationshipMap, ObjectMap
 
+
 class CiscoMemoryPool(SnmpPlugin):
     maptype = 'MemoryPool'
     relname = 'memoryPools'
